8a40391f5052fa17082613d8ea867d1edcc3b0b9
Simon Cook <si@spcook.co.uk>
2010-07-08 15:14:33 +0100

    Add configuration parser (use DOM method instead of SAX)

--- src/org/simoncook/interactus/ConfigurationLoader.java
+++ src/org/simoncook/interactus/ConfigurationLoader.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import java.net.URL;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.Attributes;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-import org.xml.sax.helpers.DefaultHandler;
+
 
 /**
  * Loads the Configuration for a Interactus Instance
@@ -18,53 +20,102 @@ import org.xml.sax.helpers.DefaultHandler;
  * @version 0.0.1
  * @since 0.0.1
  */
-public class ConfigurationLoader extends DefaultHandler
+public class ConfigurationLoader
 {
+    /**
+     * Document object to store the parsed XML file
+     */
+    private Document dom;
 
-    private String tmpString;
-
-    private void parseConfig(URL configname)
+    /**
+     * Parses a configuration
+     * @param configname URL pointing at valid Configuration XML file
+     */
+    public void parseConfig(URL configname)
     {
-        SAXParserFactory spf = SAXParserFactory.newInstance();
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         try
         {
-            SAXParser sp = spf.newSAXParser();
-            sp.parse(configname.toString(), this);
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            dom = db.parse(configname.toString());
+        }
+        catch(ParserConfigurationException pce)
+        {
+            pce.printStackTrace();
         }
         catch(SAXException se)
         {
             se.printStackTrace();
         }
-        catch(ParserConfigurationException pce)
+        catch(IOException ioe)
         {
-            pce.printStackTrace();
-	}
-        catch (IOException ie)
+            ioe.printStackTrace();
+        }
+
+        Element docEle = dom.getDocumentElement();
+        NodeList nl = docEle.getElementsByTagName("ConfigSection");
+        if(nl != null && nl.getLength() > 0)
         {
-            ie.printStackTrace();
+            for(int i = 0 ; i < nl.getLength();i++)
+            {
+                Element el = (Element)nl.item(i);
+                if(el.getAttribute("section").equals("BotCore"))
+                    parseBotCore(el);
+            }
         }
     }
 
-    // Event Handlers
-    @Override
-    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException
-    {
-		tmpString = "";
+    /**
+     * Parses the BotCore section of a configuration file.
+     * @param el Element corresponding to the BotCore component
+     */
+    private void parseBotCore(Element el){
+        NodeList nl = el.getElementsByTagName("Config");
+        if(nl != null && nl.getLength() > 0)
+        {
+            for(int i = 0; i < nl.getLength(); i++)
+            {
+                Element el2 = (Element)nl.item(i);
+                if(el2.getAttribute("name").equals("BotName"))
+                    System.out.println("Bot is now called " +
+                            el2.getFirstChild().getNodeValue());
+                else if(el2.getAttribute("name").equals("DefaultJoin"))
+                    parseChannelList(el2);
+                else if(el2.getAttribute("name").equals("Verbosity"))
+                    System.out.println("Verbosity now set to " +
+                            el2.getFirstChild().getNodeValue());
+                else
+                    System.err.println("We have something else here!");
+            }
+        }
     }
 
+    /**
+     * Parses the ChannelList section of a configuration file.
+     * @param el Element corresponding to the ChannelList component
+     */
+    private void parseChannelList(Element el){
+        Element network = (Element)el.getElementsByTagName("Network").item(0);
+        String netname = network.getFirstChild().getNodeValue();
 
-    @Override
-    public void characters(char[] ch, int start, int length) throws SAXException
-    {
-        tmpString = new String(ch,start,length);
+        System.out.println("Network to connect to: " + netname);
+        NodeList nl = el.getElementsByTagName("Channel");
+        if(nl != null && nl.getLength() > 0)
+        {
+            for(int i = 0; i < nl.getLength(); i++)
+            {
+                Element el2 = (Element)nl.item(i);
+                el2 = (Element)el2.getElementsByTagName("ChanName").item(0);
+                System.out.println("Connect to Channel: " +
+                        el2.getFirstChild().getNodeValue());
+            }
+        }
     }
 
-    @Override
-    public void endElement(String uri, String localName, String qName) throws SAXException
-    {
-        System.out.println("We now have " + tmpString + " from " + qName);
-    }
-
+    /**
+     * Temporary main
+     * @param args
+     */
     public static void main(String[] args)
     {
         ConfigurationLoader loader = new ConfigurationLoader();
